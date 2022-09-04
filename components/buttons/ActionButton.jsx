5777344f783edf2b15fac3a35f66c3b7f827c968
Abdu_ <abderraouf.dev@gmail.com>
2022-09-04 22:45:02 +0100

    add a featurnig banner section and built the carousel for the trending projects

--- components/buttons/ActionButton.jsx
+++ components/buttons/ActionButton.jsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 import buttonStyles from './ActionButton.module.css'
 
 const ActionButton = ({text, link}) => {
   return (
+    <Link href={link}>
     <a className={buttonStyles.actionBtn} href={link}>{text}</a>
+    </Link>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/navbar/Navbar'
 import HeroBanner from '../components/heroBanner/HeroBanner'
 import Products from '../components/products/Products'
+import FeaturedBanner from '../components/featuredBanner/FeaturedBanner'
+import Trending from '../components/trending/Trending'
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
 <Navbar />
 <HeroBanner />
 <Products />
+<FeaturedBanner />
+<Trending />
 </>    
   )
 }
